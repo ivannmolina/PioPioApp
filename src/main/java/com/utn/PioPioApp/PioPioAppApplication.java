951a6b3398ed806db51fb1b3ace57b831b07cec2
ivannmolina <ivan14molina@gmail.com>
2024-09-03 22:57:30 -0300

    configuracion inicial

---
+++ src/main/java/com/utn/PioPioApp/PioPioAppApplication.java
@@ -0,0 +1,13 @@
+package com.utn.PioPioApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PioPioAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PioPioAppApplication.class, args);
+	}
+
+}
